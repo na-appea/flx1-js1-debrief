39e80ce035a806f9becc2c9049a26449c30b0a03
Nana <naa.appea@gmail.com>
2023-05-10 13:02:46 -0400

    Add 3 exercises

---
+++ script.js
@@ -0,0 +1,35 @@
+//1. GenBuzz
+
+let myNumber = 18;
+
+if (typeof myNumber !== "number") {
+  console.log("This is not a number");
+} else if (myNumber % 3 == 0) {
+  console.log("Gen");
+} else if (myNumber % 5 == 0) {
+  console.log("'Buzz'");
+}
+
+//2. E-Commerce Item list
+
+let item;
+switch (item) {
+  case "shoes":
+    console.log("Shoes are $50.");
+    break;
+  case "jeans":
+    console.log("Jeans are $25.");
+    break;
+  case "hats":
+    console.log("Hats are $12.");
+    break;
+  case "socks":
+    console.log("Socks are $2.");
+    break;
+  default:
+    console.log("Invalid Item");
+}
+
+//3. Print a random integer (whole number) between 50 and 100  to the console
+
+console.log(Math.floor(Math.random() * (100 - 50)));
